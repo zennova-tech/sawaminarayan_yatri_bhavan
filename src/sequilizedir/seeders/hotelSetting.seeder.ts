f0389e0e9c40ec7f3836117f24a1c38d24379f5c
zennova-tech <info@zennova.com>
2025-06-11 16:40:31 +0530

    wip: Add admin and hotel settings seeders

---
+++ src/sequilizedir/seeders/hotelSetting.seeder.ts
@@ -0,0 +1,31 @@
+import { sequelize } from "../models";
+import HotelSettings from "../models/hotelSettings.mode";
+
+const seedHotelSettings = async () => {
+  const transaction = await sequelize.transaction();
+  try {
+    const initialSettings = {
+      total_rooms: 16,
+      available_rooms: 16,
+      booked_rooms: 0,
+      under_maintenance_rooms: 0,
+      room_amount: 1500.0,
+      room_capacity: 3,
+      mattress_amount: 300.0,
+    };
+    const existingSetting = await HotelSettings.findOne();
+    if (existingSetting) {
+      console.info(`Settings are already available`);
+      return;
+    }
+    await HotelSettings.create(initialSettings, { transaction });
+    await transaction.commit();
+    process.exit(0);
+  } catch (error) {
+    await transaction.rollback();
+    console.error(`Something went wrong while seeding settings data ${error}`);
+    process.exit(1);
+  }
+};
+
+seedHotelSettings();
