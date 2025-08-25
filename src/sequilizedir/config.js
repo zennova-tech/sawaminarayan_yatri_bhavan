88d2782505f1b2a52d924699f9ccb435db9b549b
harshika-zennova <harshikarabadiya@zennovatech.com>
2025-08-25 17:39:30 +0530

    update config file

--- src/sequilizedir/config.js
+++ src/sequilizedir/config.js
@@ -7,15 +7,15 @@ dotEnv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 module.exports = {
   url: process.env.DATABASE_URL,
   dialect: 'postgres',
-  timezone: '+05:30',
+  // timezone: '+05:30',
   pool: {
     max: 20,
     min: 0,
     acquire: 60000,
     idle: 10000,
   },
-  dialectOptions: {
-    useUTC: false, // for reading from database
-  },
+  // dialectOptions: {
+  //   useUTC: false, // for reading from database
+  // },
   logger: false,
 };
